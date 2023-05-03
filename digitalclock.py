3c8561ec6edee56d1b1821b449635a6cf2fe03c6
tejometla <dattu251@gmail.com>
2023-05-03 17:10:11 +0530

    removed welcome message

--- digitalclock.py
+++ digitalclock.py
@@ -5,4 +5,7 @@ window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
 
+Label = Label(window, font=("Arial Black",78,"bold"), bg="steelblue", fg="white")
+Label.pack(pady=100)
+
 window.mainloop()
